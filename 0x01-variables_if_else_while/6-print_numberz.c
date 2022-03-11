41f423b9d79631d73d017861d18fe16525f04792
Abdulazeez Shittu <oluwatobiabdulazeez@gmail.com>
2022-03-11 07:07:55 -0800

    Done

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - main block
+ * Description: print alphabet in lowercase
+ * Return: 0
+ */
+int main(void)
+{
+	int c = 0;
+
+	while (c <= 9)
+	{
+		putchar(48 + c);
+		c++;
+	}
+
+	putchar('\n');
+	return (0);
+}

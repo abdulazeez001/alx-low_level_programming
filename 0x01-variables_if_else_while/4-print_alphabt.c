913819926138ef5efdc9d469a4a7c4f7d50cea03
Abdulazeez Shittu <oluwatobiabdulazeez@gmail.com>
2022-03-11 06:53:11 -0800

    Done

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - main block
+ * Description: print alphabet in lowercase
+ * Return: 0
+ */
+int main(void)
+{
+	char c = 'a';
+
+	while (c <= 'z')
+	{
+		if (c == 'q' || c == 'e')
+			continue;
+		putchar(c);
+		c++;
+	}
+
+	putchar('\n');
+	return (0);
+}

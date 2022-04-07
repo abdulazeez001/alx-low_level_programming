be9436a0df1b9fc58f584cd2561399263fc596d6
Abdulazeez Shittu <oluwatobiabdulazeez@gmail.com>
2022-04-07 14:42:35 +0100

    Done

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -1,4 +1,5 @@
 #include "main.h"
+#include <stdlib.h>
 
 /**
  * _puts - prints a string, followed by a new line,
@@ -26,7 +27,7 @@ while (str[i])
 
 int _atoi(const char *s)
 {
-    int sign = 1;
+	int sign = 1;
 	unsigned long int resp = 0, firstNum, i;
 
 	for (firstNum = 0; !(s[firstNum] >= 48 && s[firstNum] <= 57); firstNum++)
--- 0x0C-more_malloc_free/test/main.c
+++ 0x0C-more_malloc_free/test/main.c
@@ -1,34 +1,11 @@
 #include <stdio.h>
-int pal(char *str)
-{
-    // char alpha1, alpha2;
-    printf("%s\n",str);
-    // while (*str)
-    // {
-    //     alpha1 = *str;
-    //     printf("%c",alpha1);
-    //     str++;
-    // } str--;
-    // while (str)
-    // {
-    //     alpha2 = *str;
-    //     str--;
-    // }
-    // printf("%c,%c",alpha1,alpha2);
-    // if (alpha1 == alpha2)
-    // {
-    //     return (1);
-    // }
-    // return (0);
-}
 
-void main (void){
-    char u[6];
-    u[0] = 'l';
-    u[1] = 'e';
-    u[2] = 'v';
-    u[3] = 'e';
-    u[4] = 'l';
-    // printf("%s\",u);
-    pal(u);
+int main(void){
+    int i = 98;
+    int *ptr;
+
+    ptr = &i;
+    *ptr = 5;
+    printf("%d,%d\n",*ptr,i);
+    return (0);
 }

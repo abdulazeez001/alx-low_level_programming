73b073f67696d0353d24d578b3987f635f5a4fcd
Abdulazeez Shittu <oluwatobiabdulazeez@gmail.com>
2022-04-07 14:21:32 +0100

    Done

--- 0x0C-more_malloc_free/test/main.c
+++ 0x0C-more_malloc_free/test/main.c
@@ -1,12 +1,34 @@
-#include <string.h>
-#include <stdlib.h>
 #include <stdio.h>
-int main(void)
+int pal(char *str)
 {
-    char *s;
+    // char alpha1, alpha2;
+    printf("%s\n",str);
+    // while (*str)
+    // {
+    //     alpha1 = *str;
+    //     printf("%c",alpha1);
+    //     str++;
+    // } str--;
+    // while (str)
+    // {
+    //     alpha2 = *str;
+    //     str--;
+    // }
+    // printf("%c,%c",alpha1,alpha2);
+    // if (alpha1 == alpha2)
+    // {
+    //     return (1);
+    // }
+    // return (0);
+}
 
-    s = malloc(12);
-    strcpy(s, "Best School");
-    printf("%s\n",s);
-    return (0);
+void main (void){
+    char u[6];
+    u[0] = 'l';
+    u[1] = 'e';
+    u[2] = 'v';
+    u[3] = 'e';
+    u[4] = 'l';
+    // printf("%s\",u);
+    pal(u);
 }

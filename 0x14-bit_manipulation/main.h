f7bc93e411e1a000f120b13d83d5f3794b78c4ce
Abdulazeez Shittu <oluwatobiabdulazeez@gmail.com>
2022-04-28 14:46:47 +0100

    Done

--- 0x14-bit_manipulation/main.h
+++ 0x14-bit_manipulation/main.h
@@ -3,7 +3,7 @@
 
 /*
  * Desc: Header file containing prototypes for all functions
- *       written in the 0x13-bit_manipulation directory.
+ *       written in the 0x14-bit_manipulation directory.
  */
 
 int _putchar(char c);
